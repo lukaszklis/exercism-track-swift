4080cc80a63e7250f91338c11150e29d93dff725
Lukasz Klis <lukasz@klis.pl>
2022-01-07 20:07:55 +0100

    Add The 1st Rule of Poetry Club task solution

--- poetry-club/Sources/PoetryClub/PoetryClub.swift
+++ poetry-club/Sources/PoetryClub/PoetryClub.swift
@@ -1,33 +1,41 @@
 import Foundation
 
+let noChar: Character = "_"
+
 func splitOnNewlines(_ poem: String) -> [String] {
-  fatalError("splitOnNewlines not implemented -- replace this error with an implementation")
+  return poem.components(separatedBy: "\n")
 }
 
 func firstLetter(_ line: String) -> Character {
-  fatalError("firstLetter not implemented -- replace this error with an implementation")
+  guard let firstCharacter = line.first else { return noChar }
+  
+  return firstCharacter
 }
 
 func capitalize(_ phrase: String) -> String {
-  fatalError("capitalize not implemented -- replace this error with an implementation")
+  return phrase.components(separatedBy: " ").map { firstLetter($0).uppercased() + $0.dropFirst(1).lowercased() }.joined(separator: " ")
 }
 
 func trimFromEnd(_ line: String) -> String {
-  fatalError("trimFromEnd not implemented -- replace this error with an implementation")
+  return line.trimmingCharacters(in: .whitespacesAndNewlines)
 }
 
 func lastLetter(_ line: String) -> Character {
-  fatalError("lastLetter not implemented -- replace this error with an implementation")
+  guard let lastCharacter = line.last else { return noChar }
+
+  return lastCharacter
 }
 
 func backDoorPassword(_ phrase: String) -> String {
-  fatalError("backDoorPassword not implemented -- replace this error with an implementation")
+  return "\(capitalize(phrase)), please"
 }
 
 func ithLetter(_ line: String, i: Int) -> Character {
-  fatalError("ithLetter not implemented -- replace this error with an implementation")
+  guard i <= line.count - 1 else { return " " }
+
+  return Array(line)[i]
 }
 
 func secretRoomPassword(_ phrase: String) -> String {
-  fatalError("secretRoomPassword not implemented -- replace this error with an implementation")
+  return "\(phrase.uppercased())!"
 }
